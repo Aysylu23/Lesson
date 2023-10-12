ee745c0e03f8e619444d0beaff413af18dc2326d
Aysylu23 <aysbad25@gmail.com>
2023-10-12 09:28:15 +0500

    example002

---
+++ Example002_Hellouser/Program.cs
@@ -0,0 +1,10 @@
+﻿//Console.Write("Введите Ваше имя");
+//string username = Console.ReadLine();
+//Console.WriteLine("Привет, ");
+//Console.WriteLine(username);
+
+Console.Write("Введите Ваше имя");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
+
