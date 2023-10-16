8947f251dfa38a31b2e4d74fdae048302f9c1516
Aysylu23 <aysbad25@gmail.com>
2023-10-16 17:57:17 +0500

    Add example011

---
+++ Example011_IntroArrayFind/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = {1, 12, 16, 48, 96, 65, 75, 4 };
+
+int n = array.Length; //возвращает количество элементов
+int find = 48;
+
+int index = 0;
+while (index < n) 
+{
+  if(array[index] == find)
+  {
+    Console.WriteLine(index);
+    break;
+  }
+  //index = index + 1;
+  index++;
+}
